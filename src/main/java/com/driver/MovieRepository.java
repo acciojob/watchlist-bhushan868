c8b8cd069962aa9bc6a215a39ad48ddab2048bc5
bhushan868 <bhushukarale643@gmail.com>
2023-05-31 17:22:37 +0530

    ok

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -1,2 +1,61 @@
-package com.driver;public class MovieRepository {
+package com.driver;
+
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
+        import java.util.Set;
+
+
+public class MovieRepository {
+    private Map<String, Movie> moviesData = new HashMap<>();
+    private Map<String, Director> directorsData = new HashMap<>();
+    private Map<String, List<String>> moviesWithDirectorMap = new HashMap<>();
+
+    public void addMovie(Movie movie) {
+        moviesData.put(movie.getName(), movie);
+    }
+
+    public void addDirector(Director director) {
+        directorsData.put(director.getName(), director);
+    }
+
+    public void addMovieDirectorPair(String movie, String director) {
+        List<String> temp = moviesWithDirectorMap.getOrDefault(director, new ArrayList<>());
+        temp.add(movie);
+        moviesWithDirectorMap.put(director, temp);
+    }
+
+    public Movie getMovieByName(String name) {
+        return moviesData.getOrDefault(name, new Movie());
+    }
+
+    public Director getDirectorByName(String name) {
+        return directorsData.getOrDefault(name, new Director());
+    }
+
+    public List<String> getMoviesByDirectorName(String director) {
+        return moviesWithDirectorMap.getOrDefault(director, new ArrayList<>());
+    }
+
+    public List<String> findAllMovies() {
+        return new ArrayList<>(moviesData.keySet());
+    }
+
+    public void deleteDirectorByName(String director) {
+        for (String movie : moviesWithDirectorMap.getOrDefault(director, new ArrayList<>())) {
+            moviesData.remove(movie);
+        }
+        moviesWithDirectorMap.remove(director);
+        directorsData.remove(director);
+    }
+
+    public void deleteAllDirectors() {
+        Set<String> directors = directorsData.keySet();
+        for (String director : directors) {
+            deleteDirectorByName(director);
+        }
+        directors.clear();;
+        moviesWithDirectorMap.clear();
+    }
 }
--- src/main/java/com/driver/MovieService.java
+++ src/main/java/com/driver/MovieService.java
@@ -1,2 +1,44 @@
-package com.driver;public class MovieService {
+package com.driver;
+
+import java.util.List;
+
+
+public class MovieService {
+    MovieRepository mr = new MovieRepository();
+
+    public void addMovie(Movie movie) {
+        mr.addMovie(movie);
+    }
+
+    public void addDirector(Director director) {
+        mr.addDirector(director);
+    }
+
+    public void addMovieDirectorPair(String movie, String director) {
+        mr.addMovieDirectorPair(movie, director);
+    }
+
+    public Movie getMovieByName(String name) {
+        return mr.getMovieByName(name);
+    }
+
+    public Director getDirectorByName(String name) {
+        return mr.getDirectorByName(name);
+    }
+
+    public List<String> getMoviesByDirectorName(String director) {
+        return mr.getMoviesByDirectorName(director);
+    }
+
+    public List<String> findAllMovies() {
+        return mr.findAllMovies();
+    }
+
+    public void deleteDirectorByName(String director) {
+        mr.deleteDirectorByName(director);
+    }
+
+    public void deleteAllDirectors() {
+        mr.deleteAllDirectors();
+    }
 }
